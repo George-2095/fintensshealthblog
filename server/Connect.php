6b2fb02692360d2a1916dbcb77120d1c924a5244
George-2095 <georgeburkadze@gmail.com>
2022-10-16 14:32:46 +0400

    first commit

--- server/Connect.php
+++ server/Connect.php
@@ -9,11 +9,6 @@ class Connect
         define('DB_PASSWORD', '');
         define('DB_NAME', 'fitnesshealthblog');
 
-        // define('DB_HOST', 'sql213.epizy.com');
-        // define('DB_USER', 'epiz_32601671');
-        // define('DB_PASSWORD', 'sY8Ohg0Yir5');
-        // define('DB_NAME', 'epiz_32601671_fitnesshealthblog');
-
         $this->conn = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
         if ($this->conn->connect_error) {
             die($this->conn->conn->connect_error);
